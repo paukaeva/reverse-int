302c060b13396abe1a4d992298bdb08bd56d3f5f
Anna Paukaeva <a.paukaeva@gmail.com>
2022-07-17 11:37:48 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) return ((n * -1).toString().split('').reverse().join('')) * 1
+  if (n > 0) return (n.toString().split('').reverse().join('')) * 1
 }
